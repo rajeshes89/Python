307aeb7487adbc16f3bc86b008fc2cf583605bd2
Rajesh T <rajesh.es89@gmail.com>
2020-10-18 13:08:13 +0530

    Update basic_log_argparse.py

--- basic_log_argparse.py
+++ basic_log_argparse.py
@@ -11,3 +11,5 @@ script_config = args_out.conf_file
 exec(open(script_config).read()) #execfile equivalent 
 log_file_name = "/tmp/logs/"+sys.argv[0].strip(".py").split("/")[-1]+time.strftime("_%Y-%m-%d_%H%M%S")+"-"+args_out.tag+".log"
 logging.basicConfig(filename = log_file_name,level=logging.DEBUG,format='%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info("test")
