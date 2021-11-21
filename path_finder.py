ecf053fa1a70b721fcd2402f949a257be915bfea
Rajesh T <rajesh.es89@gmail.com>
2021-11-21 13:21:46 +0530

    Update path_finder.py

--- path_finder.py
+++ path_finder.py
@@ -4,6 +4,8 @@ import sys
 
 def get_configs_path():
     ''' Gets the absolute path of the given file and return the full path'''
+    '''__file__ is a pointer , it contains the path of the file being executed'''
+    '''__file__ if printed from the interpreted , throws undefined error , should be executed from the file'''
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), '../configs/')
 
 def get_libs_path():
